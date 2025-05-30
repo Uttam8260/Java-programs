1668eabd83d2a7db75fca8a9da11946a7135895a
Uttam8260 <sahoouttamkumar26@gmail.com>
2025-05-30 23:53:03 +0530

    update

--- Arithmatic/Armstrong.java
+++ Arithmatic/Armstrong.java
@@ -2,25 +2,24 @@ import java.util.*;
 
 public class Armstrong {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        int original = num;
-        int digits = String.valueOf(num).length();
-        int sum = 0;
-
-        while (num > 0) {
-            int digit = num % 10;
-            sum += Math.pow(digit, digits);
-            num /= 10;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+            int original = num;
+            int digits = String.valueOf(num).length();
+            int sum = 0;
+            
+            while (num > 0) {
+                int digit = num % 10;
+                sum += Math.pow(digit, digits);
+                num /= 10;
+            }
+            
+            if (sum == original) {
+                System.out.println(original + " is an Armstrong Number.");
+            } else {
+                System.out.println(original + " is not an Armstrong Number.");
+            }
         }
-
-        if (sum == original) {
-            System.out.println(original + " is an Armstrong Number.");
-        } else {
-            System.out.println(original + " is NOT an Armstrong Number.");
-        }
-
-        sc.close();
     }
 }
---
+++ SOLID-principle/CommandDesignPattern.java
@@ -0,0 +1,125 @@
+// Client
+public class CommandDesignPattern {
+
+    // Receiver classes
+    static class Light {
+        public void turnOn() {
+            System.out.println("Light ON");
+        }
+        public void turnOff() {
+            System.out.println("Light OFF");
+        }
+    }
+
+    static class Fan {
+        public void start() {
+            System.out.println("Fan STARTED");
+        }
+        public void stop() {
+            System.out.println("Fan STOPPED");
+        }
+    }
+
+    // Command interface
+    interface Command {
+        void execute();
+    }
+
+    // Concrete Command classes
+    static class LightOnCommand implements Command {
+        private final Light light;
+
+        public LightOnCommand(Light light) {
+            this.light = light;
+        }
+
+        @Override
+        public void execute() {
+            light.turnOn();
+        }
+    }
+
+    static class LightOffCommand implements Command {
+        private final Light light;
+
+        public LightOffCommand(Light light) {
+            this.light = light;
+        }
+
+        @Override
+        public void execute() {
+            light.turnOff();
+        }
+    }
+
+    static class FanStartCommand implements Command {
+        private final Fan fan;
+
+        public FanStartCommand(Fan fan) {
+            this.fan = fan;
+        }
+
+        @Override
+        public void execute() {
+            fan.start();
+        }
+    }
+
+    static class FanStopCommand implements Command {
+        private final Fan fan;
+
+        public FanStopCommand(Fan fan) {
+            this.fan = fan;
+        }
+
+        @Override
+        public void execute() {
+            fan.stop();
+        }
+    }
+
+    // Invoker class
+    static class RemoteControl {
+        private Command command;
+
+        public void setCommand(Command command) {
+            this.command = command;
+        }
+
+        public void pressButton() {
+            command.execute();
+        }
+    }
+
+    // Main method (Client code)
+    public static void main(String[] args) {
+        // Receivers
+        Light livingRoomLight = new Light();
+        Fan ceilingFan = new Fan();
+
+        // Commands
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+        Command fanStart = new FanStartCommand(ceilingFan);
+        Command fanStop = new FanStopCommand(ceilingFan);
+
+        // Invoker
+        RemoteControl remote = new RemoteControl();
+
+        // Execute light on
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        // Execute fan start
+        remote.setCommand(fanStart);
+        remote.pressButton();
+
+        // Execute light off
+        remote.setCommand(lightOff);
+        remote.pressButton();
+
+        // Execute fan stop
+        remote.setCommand(fanStop);
+        remote.pressButton();
+    }
+}
